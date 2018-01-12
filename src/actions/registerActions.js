b9913b03e955e5523ae381565596f3828d56511f
Daniel Maragno <danielcoelho.esk@gmail.com>
2018-01-11 22:22:17 -0200

    Break the EXEC_REGISTER switch

--- src/actions/registerActions.js
+++ src/actions/registerActions.js
@@ -37,14 +37,17 @@ export function execRegister(client) {
 			switch(response.status){
 				// error
 				case 400: {
-					dispatch({type: 'EXEC_REGISTER_ERROR_ID_CUPLICATE'})
+					dispatch({type: 'EXEC_REGISTER_ERROR_ID_CUPLICATE'});
+					break;
 				}
 				case 500: {
-					dispatch({type: 'REGISTER_RESET_FLAGS'})
+					dispatch({type: 'REGISTER_RESET_FLAGS'});
+					break;
 				}
 				// ok
 				default: {
-					dispatch({type: 'EXEC_REGISTER_OK'})
+					dispatch({type: 'EXEC_REGISTER_OK'});
+					break;
 				}
 			}
 		})
